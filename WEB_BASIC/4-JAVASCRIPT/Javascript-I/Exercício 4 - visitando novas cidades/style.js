69dfd65257245832c69ee5ba7e0b725873e37fd2
Augusto Kayali <kayaliaugusto@gmail.com>
2025-07-04 20:03:52 -0300

    Add files via upload

---
+++ WEB_BASIC/4-JAVASCRIPT/Javascript-I/Exercício 4 - visitando novas cidades/style.js
@@ -0,0 +1,15 @@
+const turista  = prompt("digite seu nome");
+let cidades = ""
+let contagem = 0
+let continuar = prompt("Você visitou alguma cidade? (Sim/Não)")
+while (continuar === "Sim" || continuar === "sim" || continuar === "s" || continuar === "S") {
+    let cidade = prompt("Qual é o nome da cidade visitada?")
+  cidades += " - " + cidade + "\n"
+  contagem++
+  continuar = prompt("Você visitou alguma outra cidade? (Sim/Não)")
+}
+alert(
+  "Turista: " + turista +
+  "\nQuantidade de cidades visitadas: " + contagem +
+  "\nCidades visitadas:\n" + cidades
+)
---
+++ WEB_BASIC/4-JAVASCRIPT/Javascript-I/Exercício 5 - Menu Interativo e Controle financeiro/2.Controle financeiro/style.js
@@ -0,0 +1,25 @@
+let saldo = parseFloat(prompt("informe saldo atual"));
+let opcao = "";
+
+do {
+  opcao = prompt(
+    "Saldo disponível: R$ " + saldo +
+    "\n1 - Adicionar dinheiro" +
+    "\n2 - Remover dinheiro" +
+    "\n3 - Sair"
+  )
+    switch (opcao) {
+        case "1":
+        saldo += parseFloat(prompt("informe o valor a ser adicionado"));
+        break;
+        case "2":
+        saldo -= parseFloat(prompt("informe o valor a ser retirado"));
+        break;
+        case "3":
+        alert("saindo...");
+        break;
+        default:
+        alert("entrada invalida")
+        break
+}
+} while (opcao !== "3")
